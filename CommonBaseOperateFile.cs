70e75d186c666251ec70f7e4800b358718e7eb21
Tony law <13392226@qq.com>
2021-09-15 11:49:31 +0800

    Add files via upload

---
+++ CommonBaseOperateFile.cs
@@ -0,0 +1,226 @@
+﻿using System; 
+using System.Collections.Generic; 
+using System.Drawing; 
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;   
+using System.Configuration;
+
+using System.Text.RegularExpressions;
+using Newtonsoft.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public partial class CommonBase
+    { 
+
+        public enum LoggerMode
+        {
+            DEBUG=-1,
+            INFO = 0,
+            WARNNING = 1,
+            ERROR = 2,
+            FATAL = 3
+        }
+        public static bool SaveImage(string Path1, string FileName, string PhotoBase64)
+        {
+            string PathFileName = Path.Combine(Path1, FileName);
+            byte[] bt = Convert.FromBase64String(PhotoBase64);
+
+            if (System.IO.Directory.Exists(Path1) == false)
+            {
+                System.IO.Directory.CreateDirectory(Path1);
+            }
+            try
+            {
+                File.WriteAllBytes(PathFileName, bt);
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+        public static string ReadConfigJson(string  PathFileName)
+        {
+            if(File.Exists(PathFileName))
+            {
+                string content = File.ReadAllText(PathFileName, Encoding.UTF8);
+                return content;
+            }else
+            {
+                return string.Empty;
+            }
+           
+        }
+        public static bool SaveDataJson(string FileContent,string PathFile)
+        {
+            StreamWriter writer = null;
+            try
+            { 
+                writer = new StreamWriter(PathFile, false, System.Text.Encoding.GetEncoding("UTF-8"));
+                writer.Write(FileContent);
+            }
+            catch
+            {
+                return false;
+            }
+            finally
+            {
+                if (writer != null)
+                    writer.Close();
+            }
+
+            return true;
+        }
+        public static bool SaveDataJson(string fileContent, string path, string targetFileName)
+        {
+            StreamWriter writer = null;
+            try
+            {
+                if (System.IO.Directory.Exists(path) == false)
+                {
+                    System.IO.Directory.CreateDirectory(path);
+                }
+                string PathFile = Path.Combine(path, targetFileName);
+                writer = new StreamWriter(PathFile, false, System.Text.Encoding.GetEncoding("UTF-8"));
+                writer.Write(fileContent);
+            }
+            catch
+            {
+                return false;
+            }
+            finally
+            {
+                if (writer != null)
+                    writer.Close();
+            }
+
+            return true;
+        }
+
+        public static string ReadDataFromJson( string path, string targetFileName)
+        {
+            string PathFileName = Path.Combine(path, targetFileName);
+            if (File.Exists(PathFileName))
+            {
+                string content = File.ReadAllText(PathFileName, Encoding.UTF8);
+                return content;
+            }
+            else
+            {
+                return string.Empty;
+            }  
+        }
+        public static string ReadDataFromJson(string PathFileName)
+        { 
+            if (File.Exists(PathFileName))
+            {
+                string content = File.ReadAllText(PathFileName, Encoding.UTF8);
+                return content;
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+        public static bool CopyToFolder(string TargetPath, string TargetFileName, string SourcePathFileName)
+        {
+            string TargetPathFileName = Path.Combine(TargetPath, TargetFileName);
+             
+            if (!System.IO.Directory.Exists(TargetPath) )
+            {
+                System.IO.Directory.CreateDirectory(TargetPath);
+            }
+            try
+            {
+                File.Copy(SourcePathFileName, TargetPathFileName, true);
+                return true;
+            }
+            catch
+            {
+                return false;
+            } 
+        }
+
+        #region 
+        public static bool WriteLogNtimes(string LineContent, LoggerMode loggerMode)
+        {
+            string dateTimeLineLog = string.Format("[{0:yyyy-MM-dd HH:mm:ss fff}] {1}", DateTime.Now, LineContent);
+            Logger logger = new Logger { LoggerLine = dateTimeLineLog, loggerMode = loggerMode }; 
+            LoggerQueueHelper.instance.AddData(logger); 
+            bool AnalysisStatus = LoggerQueueHelper.instance.AnalysisStatus; 
+            if (!AnalysisStatus)
+            { 
+                if (LoggerQueueHelper.instance.Getcount() > 0)
+                { 
+                    LoggerQueueHelper.instance.PostAnalysis();
+                }
+            }
+            return true;
+        }
+        public static bool OperateLoger(string LineContent)
+        {
+            bool result = WriteLogNtimes(LineContent, LoggerMode.INFO);  //修改为强行加入LoggerMode.INFO
+            return result;
+        } 
+        public static void OperateDateLoger(string LoggerLineContent, object objData)
+        {
+            string ObjectData = JsonCompress(JsonConvert.SerializeObject(objData));
+            string ItemContent = string.Format("[{0}] [DATA]:[{1}]", LoggerLineContent, ObjectData);
+            OperateDateLoger(ItemContent);
+            return ;
+        }
+        public static void OperateDateLoger(string LoggerLineContent, object objData,LoggerMode loggerMode)
+        {
+            string ObjectData = JsonCompress(JsonConvert.SerializeObject(objData));
+            string ItemContent = string.Format("[{0}] [{1}]  [OBJECT DATA]:[{2}]", loggerMode.ToString(), LoggerLineContent, ObjectData);
+            WriteLogNtimes(ItemContent, loggerMode); 
+            return;
+        }
+        #endregion
+
+        #region  Date Loger
+        public static bool OperateDateLoger(string LineContent)
+        {
+            bool result = WriteLogNtimes(LineContent, LoggerMode.INFO);  //修改为强行加入LoggerMode.INFO
+            return result;
+        }
+        public static bool OperateDateLoger(string LineContent,LoggerMode loggerMode)
+        {
+            bool result = WriteLogNtimes(LineContent, loggerMode);  //修改为强行加入LoggerMode.INFO
+            return result;
+        }
+        public static bool EntryImageLoger(string LineEntryImage)
+        {
+            bool result = WriteLogNtimes(LineEntryImage, LoggerMode.INFO);
+            return result;
+        }
+        #endregion
+        public static string GetSystemDriveLogPath()
+        {
+            string path;
+            string Systemdrive = Environment.GetEnvironmentVariable("systemdrive");
+            Systemdrive = string.Format("{0}\\", Systemdrive);
+            path = Path.Combine(Systemdrive, "log");
+             
+            if (System.IO.Directory.Exists(path) == false)
+            {
+                System.IO.Directory.CreateDirectory(path);
+            }
+            return path;
+        }
+
+        public static bool IsConsoleOrWinformPath()
+        {
+            string EnvPath = System.Environment.CurrentDirectory.TrimEnd('\\');
+            string BaseDirPath = AppDomain.CurrentDomain.BaseDirectory.TrimEnd('\\'); 
+            return EnvPath == BaseDirPath;
+        }
+    }
+    
+
+}
---
+++ LoggerQueueHelper.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using static Common.CommonBase;
+
+namespace Common
+{
+    public class Logger
+    {
+        public LoggerMode loggerMode { get; set; } = LoggerMode.INFO;
+        public string LoggerLine { get; set; } = string.Empty;
+    }
+    public class LoggerQueueHelper
+    {
+        private LoggerQueueHelper()
+        { 
+        } 
+        private static string _LogPath;
+        public static string LogPath
+        {
+            get
+            {
+                if (MemoryCacheHelper.Contains("LogPath") == false)
+                {
+                    DateTimeOffset thisOffsetTime = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day,23,59,59);
+                    string logPath = Path.GetFullPath("./"); 
+                    MemoryCacheHelper.Set("LogPath", logPath, thisOffsetTime);
+                    string path1 = logPath;
+                    if (!Directory.Exists(path1))
+                    {
+                        Directory.CreateDirectory(path1);
+                    }
+                    _LogPath = logPath;
+                }
+                else
+                {
+                    _LogPath = MemoryCacheHelper.GetCacheItem<string>("LogPath");
+                }
+                return _LogPath;
+            }
+        }
+        #region  
+        private static object lockObj1 = new object();
+        private static void Log(Logger logger)
+        {
+            lock (lockObj1)
+            {
+                string filePath = Path.Combine(LogPath, "log");
+                string filename = CheckLogFile(logger.loggerMode,500); 
+                string pathFilename = Path.Combine(filePath, filename);
+                 
+                ReaderWriterLockSlim readerWriterLockSlim = new ReaderWriterLockSlim(); //ref:https://www.cnblogs.com/tianma3798/p/8252553.html
+                try
+                {
+                    readerWriterLockSlim.EnterWriteLock();
+                    using (StreamWriter w = new StreamWriter(pathFilename, true, Encoding.UTF8))
+                    {
+                        w.WriteLine(logger.LoggerLine);
+                        w.Close();
+                    }
+                }
+                catch
+                {
+                    throw;
+                }
+                finally
+                {
+                    readerWriterLockSlim.ExitWriteLock();   //
+                }
+                
+            }
+        }
+        #endregion
+        private static object lockObj2 = new object();
+
+        private static void ErrorLog(string msg)
+        {
+            lock (lockObj2)
+            {
+                string filePath = Path.Combine(LogPath, "log");  
+                string filename = CheckLogFile(LoggerMode.ERROR,500);
+                string errPathfileName = Path.Combine(filePath, filename);
+                if (!File.Exists(errPathfileName))
+                {
+                    FileStream fs = File.OpenWrite(errPathfileName);
+                    fs.Flush();
+                    fs.Close();
+                }
+
+                ReaderWriterLockSlim readerWriterLockSlim = new ReaderWriterLockSlim(); //ref:https://www.cnblogs.com/tianma3798/p/8252553.html
+                try
+                { 
+                    readerWriterLockSlim.EnterWriteLock();
+                    using (StreamWriter w = new StreamWriter(errPathfileName, true, Encoding.UTF8))
+                    {
+                        w.WriteLine(msg); 
+                        w.Close();
+                    }
+                }
+                catch
+                {
+                    throw;
+                }
+                finally
+                {
+                    readerWriterLockSlim.ExitWriteLock();   //
+                }
+                
+            }
+        }
+
+        public bool AnalysisStatus = false;
+
+        public static LoggerQueueHelper instance = new LoggerQueueHelper();
+        
+        private ConcurrentQueue<Logger> t = new ConcurrentQueue<Logger>();
+        /// <summary>
+        /// add to queue
+        /// </summary>
+        /// <param name="tip"></param>
+        public int AddData(Logger tip)
+        {
+            t.Enqueue(tip);
+            return t.Count;
+        }
+
+        /// <summary>
+        /// get the the first queue
+        /// </summary>
+        /// <returns></returns>
+        public Logger Get()
+        {
+            Logger logger = new Logger();
+            return t.TryPeek(out logger) ? logger : null;
+        }
+
+        /// <summary>
+        ///get from queue and calc
+        /// </summary>
+        public void PostAnalysis()
+        {
+            while (t.Count > 0)
+            {
+                try
+                {
+                    AnalysisStatus = true;
+
+                    Logger outresult = new Logger();
+
+                    if (t.TryDequeue(out outresult))
+                    {
+                        Log(outresult);
+                    }
+                    else
+                    {
+                        ErrorLog(string.Format("[{0:yyyy-MM-dd HH:mm:ss}][ERROR (TryDequeue error)]", DateTime.Now));
+                    }
+                }
+                catch (Exception ex)
+                {
+                    AnalysisStatus = false;
+                    ErrorLog(string.Format("[{0:yyyy-MM-dd HH:mm:ss}][FUNC::LoggerQueueHelper.PostAnalysis][EXCEPTION] [{1}]", DateTime.Now, ex.Message));
+                    throw;
+                }
+            }
+            AnalysisStatus = false;
+        }
+
+        /// <summary>
+        /// get the queue total count
+        /// </summary>
+        /// <returns></returns>
+        public int Getcount()
+        {
+            return t.Count;
+        }
+
+        /// <summary>
+        /// 检查log文件
+        /// </summary>
+        /// <param name="loggerMode"></param>
+        /// <param name="fileMaxSize">建议500 单位Kb</param>
+        /// <returns></returns>
+        public static string CheckLogFile(LoggerMode loggerMode,long fileMaxSize)
+        {
+            int fileIndex = 0;
+            fileMaxSize = fileMaxSize * 1000; 
+
+            string filePath = Path.Combine(LogPath, "log");
+            string logExtend = ".log";
+            if (!Directory.Exists(filePath))
+            {
+                Directory.CreateDirectory(filePath);
+            }
+
+            string name = string.Format("Logger{0}{1:yyyyMMdd}", LoggerMode.INFO, DateTime.Now);
+            switch (loggerMode)
+            {
+                case LoggerMode.INFO:
+                    name = string.Format("Logger{0}{1:yyyyMMdd}", LoggerMode.INFO, DateTime.Now); 
+                    break;
+                case LoggerMode.DEBUG:
+                    name = string.Format("Logger{0}{1:yyyyMMdd}", LoggerMode.DEBUG, DateTime.Now); 
+                    break;
+                case LoggerMode.ERROR:
+                    name = string.Format("Logger{0}{1:yyyyMMdd}", LoggerMode.ERROR, DateTime.Now); 
+                    break;
+                case LoggerMode.FATAL:
+                    name = string.Format("Logger{0}{1:yyyyMMdd}", LoggerMode.FATAL, DateTime.Now); 
+                    break;
+                case LoggerMode.WARNNING:
+                    name = string.Format("Logger{0}{1:yyyyMMdd}", LoggerMode.WARNNING, DateTime.Now); 
+                    break;
+                default:
+                    break;
+            }
+            string filename = string.Format("{0}_{1}{2}", name, fileIndex, logExtend);
+            string pathFileName = Path.Combine(filePath, filename);
+
+            if (!File.Exists(pathFileName))
+            {
+                FileStream fs = File.OpenWrite(pathFileName);
+                fs.Flush();
+                fs.Close();
+                return filename;
+            }
+            else
+            {
+                //处理文件过大问题,超过200K,则使用序数文件 >2000000 (>200K) 
+                while(true){
+                     
+                    FileInfo fileInfo = new FileInfo(pathFileName); 
+                    if(fileInfo.Length < fileMaxSize)
+                    {
+                        return fileInfo.Name; 
+                    }
+                    else
+                    {
+                        fileIndex++;
+
+                        filename = string.Format("{0}_{1}{2}", name, fileIndex, logExtend);
+                        pathFileName = Path.Combine(filePath, filename);
+
+                        if (!File.Exists(pathFileName))
+                        {
+                            FileStream fs = File.OpenWrite(pathFileName);
+                            fs.Flush();
+                            fs.Close();
+
+                            return filename;
+                        } 
+                    }
+                    
+
+                    if (fileIndex > 99)  //The max files 99
+                        break;
+                }
+            } 
+            return filename;
+        }
+    }
+}
